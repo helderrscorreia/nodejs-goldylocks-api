832625133e88e8b2e89139c9aed4712e14e172f2
Hélder Correia <helderrscorreia@gmail.com>
2020-08-10 08:30:24 +0100

    Initial working version

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
 const Goldylocks = require('../lib/goldylocks');
-let gl = new Goldylocks();
+const gl = new Goldylocks();
 gl.setup("empresademonstrativa","57b16c9ff6a18241f5aea066d3955c972e27ed48");
+gl.request('clientes', {p:"alves"},{},data => console.table(data.length));
